a2fe1a6e1f36b4bb0448b28d64b1a9200e591584
Joe Thomas <joe.p.thomas92@gmail.com>
2017-02-21 17:40:04 -0800

    implement interval to play selected tiles, basic functionality in place

--- components/matrix.jsx
+++ components/matrix.jsx
@@ -7,6 +7,7 @@ class Matrix extends React.Component {
   constructor(props) {
     super();
     this.board = this.buildBoard();
+    this.activeTiles = this.buildActiveTiles();
     this.handleSelect = this.handleSelect.bind(this);
 
     let col = 0;
@@ -14,27 +15,39 @@ class Matrix extends React.Component {
       col += 1;
       col %= 12;
       this.playCol(col);
-    }, 1000);
+    }, 200);
   }
 
   buildBoard() {
     let board = new Array(12);
     for (let row = 0; row < 12; row++) {
-      board[row] = [];
+      board[row] = new Array(12);
       for (let col = 0; col < 12; col++) {
-        board[row].push(<div className='tile'/>);
+        let pos = `${row},${col}`;
+        board[row][col] = (<div id={pos} className='tile'/>);
+      }
+    }
+    return board;
+  }
+
+  buildActiveTiles() {
+    let board = new Array(12);
+    for (let row = 0; row < 12; row++) {
+      board[row] = new Array(12);
+      for (let col = 0; col < 12; col++) {
+        board[row][col] = false;
       }
     }
     return board;
   }
 
   playCol(col) {
-    let freq = 1000;
-    this.board.forEach((row) => {
-      if (row[col].props.className === 'tile selected') {
+    let freq = 440;
+    this.activeTiles.forEach((row) => {
+      if (row[col]) {
         new Sound(this.props.context, freq);
       }
-      freq -= 100;
+      freq = freq * Math.pow(2, -1/12);
     });
   }
 
@@ -44,6 +57,13 @@ class Matrix extends React.Component {
     } else if (e.target.className === 'tile selected') {
       e.target.className = 'tile';
     }
+
+    if (e.target.id) {
+      let row = parseInt(e.target.id.split(',')[0]);
+      let col = parseInt(e.target.id.split(',')[1]);
+      this.activeTiles[row][col] = !this.activeTiles[row][col];
+    }
+
     e.stopPropagation();
   }
 
